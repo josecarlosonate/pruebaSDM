5b61985154d4e5c6ce93fe3b6dc560fb24d25e23
Jose Rodriguez <jonate@nexura.com>
2022-02-11 23:39:20 -0500

    feat:crud usuarios

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const mysqlConnection = require('../database');
 
 /* Obtener todos los usuarios */
-router.get('/', (req, res) => {
-    mysqlConnection.query('SELECT * FROM users', (err, rows, fields) => {
+router.get('/api/user', (req, res) => {
+    mysqlConnection.query('SELECT * FROM users WHERE state = ?', [1], (err, rows, fields) => {
         if (!err) {
             res.json(rows);
         } else {
@@ -15,9 +15,9 @@ router.get('/', (req, res) => {
 });
 
 /* Obtener usuario por id */
-router.get('/:id', (req, res) => {
+router.get('/api/user/:id', (req, res) => {
     const { id } = req.params;
-    mysqlConnection.query('SELECT * FROM users WHERE id = ?', [id], (err, rows, fields) => {
+    mysqlConnection.query('SELECT * FROM users WHERE id = ? AND state = ?', [id, 1], (err, rows, fields) => {
         if (!err) {
             res.json(rows[0]);
         } else {
@@ -26,7 +26,49 @@ router.get('/:id', (req, res) => {
     });
 });
 
-/* Ingresar nuevo usuario */
-router.post();
+/* Ingresar  usuario */
+router.post('/api/user/add', (req, res) => {
+    let { id, name, document, create_at, update_at } = req.body;
+    if (!id) {
+        id = 0;
+    }
+    console.log(req.body);
+    const query = `
+        CALL userAddOrEdit (?, ?, ?, ?, ?);
+    `;
+    mysqlConnection.query(query, [id, name, document, create_at, update_at], (err, rows, fields) => {
+        if (!err) {
+            res.json({ Code: "Ok", Status: 'Realizado con exito' });
+        } else {
+            res.json({ Code: "Error", Status: err });
+        }
+    });
+});
+
+/* Editar usuario */
+router.put('/api/user/edit/:id', (req, res) => {
+    const { name, document, create_at, update_at } = req.body;
+    const { id } = req.params;
+    const query = 'CALL userAddOrEdit (?, ?, ?, ?, ?);';
+    mysqlConnection.query(query, [id, name, document, create_at, update_at], (err, rows, fields) => {
+        if (!err) {
+            res.json({ Code: "Ok", Status: 'Actualizado con exito' });
+        } else {
+            res.json({ Code: "Error", Status: err });
+        }
+    });
+});
+
+/* Eliminar usuario */
+router.delete('/api/user/delet/:id', (req, res) => {
+    const { id } = req.params;
+    mysqlConnection.query('DELETE FROM users WHERE id = ?', [id], (err, rows, fields) => {
+        if (!err) {
+            res.json({ Code: "Ok", Status: 'Eliminado con exito' });
+        } else {
+            res.json({ Code: "Error", Status: err });
+        }
+    });
+});
 
 module.exports = router;
